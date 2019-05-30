226fc0aa57daa5ac4fbae0dbf759184b4abd9cac
Daniel Mihai GORGAN <dgorgan@bitdefender.com>
2019-05-30 20:54:19 +0300

    maps

--- src/components/Places/Place.js
+++ src/components/Places/Place.js
@@ -4,18 +4,21 @@ import Button from "@material-ui/core/Button";
 
 import '../../styles/css/Course.css'
 import {Link} from "react-router-dom";
+import connect from "react-redux/es/connect/connect";
+import {getLink} from "../../actions/Map";
 
 class Place extends React.Component {
 
-    // asta e gen onCreate unde se apeleaza o functie
     componentWillMount() {
-        //apel functie
+        this.props.getLink()
     }
+
     render() {
         // const placesId = this.props.match.params.placesId;
         const place = this.props.location.state;
-
+        let link = `https://www.google.com/maps/embed/v1/place?q=${place.name}&key=AIzaSyAbBGJtRp255cAdDqpCMd0JBAJbINn0kRs`;
         return (
+
             <div className='course panel'>
                 <div className="course__title">{place.name}</div>
 
@@ -31,15 +34,23 @@ class Place extends React.Component {
                         {place.description}
                     </div>
                     <div className="course__meta--semester">
-                        <span className="course__meta--bold">Contact</span>&nbsp;&nbsp;&nbsp;
-                        <ul>
-                            <li className="list__contact"><span
-                                className="place__li--bold">Telefon:</span> {place.contact.phone.toString()}</li>
-                            <li className="list__contact"><span
-                                className="place__li--bold">Email:</span> {place.contact.email.toString()}</li>
-                        </ul>
-
+                    <span className="course__meta--bold">Contact</span>&nbsp;&nbsp;&nbsp;
+                    <ul>
+                        <li className="list__contact"><span
+                            className="place__li--bold">Telefon:</span> {place.contact.phone.toString()}</li>
+                        <li className="list__contact"><span
+                            className="place__li--bold">Email:</span> {place.contact.email.toString()}</li>
+                    </ul>
+                </div>
+                    <div className="course__meta--semester">
+                        <span className="course__meta--bold">Location:</span>&nbsp;&nbsp;&nbsp;
+                        <iframe width="600"
+                                height="450"
+                                style={{border: "0"}}
+                                src={link}
+                                allowFullScreen>a</iframe>
                     </div>
+
                 </div>
                 <Link to={{
                     pathname: `/places/${place.id}/form`,
@@ -58,4 +69,8 @@ class Place extends React.Component {
     }
 }
 
-export default Place;
+const mapStateToProps = state => ({
+    link: state.link.link
+});
+
+export default connect(mapStateToProps, {getLink})(Place);
