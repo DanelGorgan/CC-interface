1f9e1058db57a4c1041ac41aa9995c3c440e8557
Gorgan Răzvan-Gabriel <gabigorgan@gmail.com>
2019-05-31 07:27:26 +0300

    send email

--- src/actions/ReservationPlaces.js
+++ src/actions/ReservationPlaces.js
@@ -19,13 +19,32 @@ export const getReservations = () => dispatch => {
 
 };
 
-export const updateReservation = (id, response) => dispatch => {
+export const updateReservation = (id, response, email) => dispatch => {
     let body = {};
     if (response && response.trim() === "Accept") {
         body.status = "approved"
+        console.log(email)
+        var data = {
+            "to": email,
+            "subject": "Quilly - Rezervare",
+            "mess": `Rezervarea ta a fost ${body.status}`
+        }
+        console.log('updateReservation')
+        axios
+        .post('https://tema4-send-email.azurewebsites.net/api/HttpTrigger1?code=jSw3kMS4yV50NE8clvBMCAOY72Ak6tctIu/PNLAatQ/Fd5HzRHo1BA==', body)
     } else {
         body.status = "declined"
+        var data = {
+            "to": email,
+            "subject": "Quilly - Rezervare",
+            "mess": `Rezervarea ta a fost ${body.status}`
+        }
+        axios
+        .post('https://tema4-send-email.azurewebsites.net/api/HttpTrigger1?code=jSw3kMS4yV50NE8clvBMCAOY72Ak6tctIu/PNLAatQ/Fd5HzRHo1BA==', body)
     }
+
+    console.log('updateReservation')
+
     axios
         .put(`https://datastore-project-236517.appspot.com/reservations/${id}`, body)
         .then(result => {
